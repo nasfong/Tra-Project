05e26db94baa0c2e5d5c2217d399200c6f63c11f
Nas Fong <106638978+nasfong@users.noreply.github.com>
2025-09-25 19:55:56 +0700

    update

--- src/lib/constant.tsx
+++ src/lib/constant.tsx
@@ -1,6 +1,6 @@
-const API_URL = window.__ENV__?.VITE_API_URL || "http://localhost:5000";
+const API_URL = window.__ENV__?.VITE_API_URL || import.meta.env.VITE_API_URL;
 
-const imageUrl = window.__ENV__?.VITE_IMAGE_URL
+const imageUrl = window.__ENV__?.VITE_IMAGE_URL || import.meta.env.VITE_IMAGE_URL
 
 const stock = [
   { id: 1, name: "In Stock" },
