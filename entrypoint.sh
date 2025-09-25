dd783b9465e092731bb725e3d793eadc4e6fbcd6
Nas Fong <106638978+nasfong@users.noreply.github.com>
2025-09-25 19:21:33 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Creating runtime env file..."
 cat <<EOF > /usr/share/nginx/html/env.js
 window.__ENV__ = {
   VITE_API_URL: "${VITE_API_URL}",
-  VITE_IMAGE_URL: "${VITE_IMAGE_URL}"
+  VITE_IMAGE_URL: "${VITE_IMAGE_URL}",
 };
 EOF
 
